9dc2c7a97769a1587cd2c72d9fb1f971f40d0258
alex <lalexis.covarrubiasl@gmail.com>
2022-05-17 15:41:15 -0500

    se craron tres archivos que fue un html, css , js

---
+++ README.md
@@ -0,0 +1,1 @@
+# En este proyecto se creo para aprender a usar git ,  se crearon 3 archivos que tienen que ser subidos a github 
---
+++ main.js
@@ -0,0 +1,6 @@
+<script>
+var age = 18;
+var ella = 'El señor';
+var el = 'La señora';
+document.write(ella + " tiene " + age + " años\n");
+document.write(el + " tiene " + age + " años");
